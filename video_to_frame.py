f6b4f7bafaf92aee37206b561a6472b1d73eab1e
Josh <josh4050@googlemail.com>
2022-05-21 14:06:38 +0100

    Initial commit

---
+++ video_to_frame.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import cv2 as cv
+from glob import glob
+
+def save_frame(video_path, save_dir):
+    name = video_path.split("/")[-1].split(".")[0]
+    save_path = os.path.join(save_dir, name)
+    create_dir(save_path)
+
+    cap = cv.VideoCapture(video_path)
+    idx = 0
+
+    while True:
+        ret, frame = cap.read()
+
+        if ret == False:
+            cap.release()
+            break
+        
+        cv.imwrite(f"{save_path}/{idx}.png", frame)
+
+        idx += 1
+
+
+def create_dir(path):
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"Error: Creating directory {path}")
+
+
+if __name__ == "__main__":
+    video_paths = glob("videos/*")
+    save_dir = "save"
+
+    for path in video_paths:
+        save_frame(path, save_dir)
